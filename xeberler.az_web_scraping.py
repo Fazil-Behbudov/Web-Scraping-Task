8d1fdfab1db829b9a2618b506c6d49cb69aac6b8
Behbudov Fazil <behbudovfazil11@gmail.com>
2025-07-24 23:14:47 +0400

    Update xeberler.az_web_scraping.py

--- xeberler.az_web_scraping.py
+++ xeberler.az_web_scraping.py
@@ -2,27 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 from datetime import datetime
-News = []
-for i in range(1,41):
-    url = f"https://xeberler.az/new/content/all/{i}"
-    get_content = requests.get(url).content
-    page = BeautifulSoup(get_content,'lxml')
-    news_list = page.find_all('div',class_='sec-topic')
 
-    for news in news_list:
-        scrap_time = datetime.now()
-        header = news.find('h3').text
-        link = news.a['href']
-        date = news.find('div',class_='time').text.strip()
-        photo_url = news.find('img',class_="img-thumbnail cat-big-img")['src']
-        get_content2 = requests.get(link).content
-        news_page = BeautifulSoup(get_content2,'lxml')
-        text = news_page.find('div',class_='time').text
-        index = text.index('/') + 1
-        news_time = text[index:]
-        topic = news_page.find('h1').text
-        info = news_page.find('div',class_="news-details-all").text.replace('\n','')
-        News.append([header,link,photo_url,date,news_time,topic,info,scrap_time])
+def scrape_xeberler():
+    News = []
+    for i in range(1, 41):
+        url = f"https://xeberler.az/new/content/all/{i}"
+        get_content = requests.get(url).content
+        page = BeautifulSoup(get_content, 'lxml')
+        news_list = page.find_all('div', class_='sec-topic')
 
-df = pd.DataFrame(News,columns=['Başlıq','Link','Foto url','Tarix','Saat','Kateqoriya','Xəbər','Scrap tarixi'])
-df.to_csv("Xəbərlər_az.csv",index=False)
+        for news in news_list:
+            scrap_time = datetime.now()
+            header = news.find('h3').text
+            link = news.a['href']
+            date = news.find('div', class_='time').text.strip()
+            photo_url = news.find('img', class_="img-thumbnail cat-big-img")['src']
+            get_content2 = requests.get(link).content
+            news_page = BeautifulSoup(get_content2, 'lxml')
+            text = news_page.find('div', class_='time').text
+            index = text.index('/') + 1
+            news_time = text[index:]
+            topic = news_page.find('h1').text
+            info = news_page.find('div', class_="news-details-all").text.replace('\n', '')
+            News.append([header, link, photo_url, date, news_time, topic, info, scrap_time])
+
+    df = pd.DataFrame(News, columns=[
+        'Başlıq', 'Link', 'Foto url', 'Tarix', 'Saat', 'Kateqoriya', 'Xəbər', 'Scrap tarixi'
+    ])
+    df.to_csv("Xəbərlər_az.csv", index=False)
+if __name__ == "__main__":
+    scrape_xeberler()
